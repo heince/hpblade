b4049318681ed5d4662f220944770e0137e7380f
Heince Kurniawan <heince@gmail.com>
2012-11-18 22:22:37 +0800

    add function to show by enclosure name

--- lib/Server/MAC.pm
+++ lib/Server/MAC.pm
@@ -40,6 +40,43 @@ sub showmac{
 	}
 }
 
+sub showmac_byenc{
+	my $self = shift;
+	
+	my ($enc) = @_;
+	my @encs = split ',' => $enc;
+	my $cmd =  $self->set_showmac_cmd();
+	
+	my $config = $self->getConfigHash();
+	for (keys $config){
+		for my $blade(keys $$config{$_}){
+			my @tmp = grep {/\b$blade\b/} @encs;
+			if(@tmp){
+				my $ip = $$config{'blades'}{$blade}{'ip'};
+				my $username = $$config{'blades'}{$blade}{'user'};
+				my $password = $$config{'blades'}{$blade}{'password'};
+				my $timeout = $$config{'sshtimeout'} || 5;
+				my $port = $$config{'sshport'} || 22;
+				
+				say "Running $cmd on $ip";
+				my $ssh2 = $self->connect_ssh($ip, $username, $password, $port, $timeout);
+				
+				my $result = $self->exec_ssh_cmd($ssh2, $cmd);
+				my @output = split '\n' => $result;
+	            for (@output){
+	            	if(/\bServer Name\b/){
+	                	print "\n";
+	                    say;
+	                }
+	                if(/([0-9A-F]{2}[:-]){5}([0-9A-F]{2})/i){
+	                	say;
+	                }
+				}
+			}
+		}
+	}
+}
+
 sub get_slot{
 	my $self = shift;
 	
